78ed5f777e28915366deccc35247b06d95eb763d
Keegan Leary <35752455+learyjk@users.noreply.github.com>
2024-07-18 06:51:11 -0700

    vite + webflow setup

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import svelteLogo from "./assets/svelte.svg";
+  import viteLogo from "/vite.svg";
+  import Counter from "./lib/Counter.svelte";
+
+  console.log(webflow);
+
+  webflow.notify({ type: "Info", message: "Hello from Svelte!" });
+</script>
+
+<main>
+  <h1>Pattern Bae</h1>
+</main>
